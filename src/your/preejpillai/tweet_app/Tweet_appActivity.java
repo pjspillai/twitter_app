85940a6ec349f6ba12cc1e7259971a116af03d90
Preeti <preetipillai@gmail.com>
2012-02-15 02:42:07 -0800

    initial one

---
+++ src/your/preejpillai/tweet_app/Tweet_appActivity.java
@@ -0,0 +1,13 @@
+package your.preejpillai.tweet_app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Tweet_appActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
